0ce03dad558e8698693279258f35a5cf663860c3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-05-18 16:44:45 -0400

    solution

--- cypress/e2e/menu-spec.cy.js
+++ cypress/e2e/menu-spec.cy.js
@@ -11,7 +11,7 @@ describe('Menu', () => {
     cy.visit('public/index.html')
   })
 
-  it.only('picks random top-menu link', () => {
+  it('picks random top-menu link', () => {
     // confirm we see the main page with the "Index page" text
     // https://on.cypress.io/contains
     cy.contains('main', 'Index page')
@@ -20,31 +20,42 @@ describe('Menu', () => {
     // and cy.sample from the cypress-map plugin
     // click on the picked navigation link
     cy.get('nav a').sample().click()
+    // confirm the URL changes to a or b or c or d top-level page
     cy.location('pathname').should('match', /\/public\/[a-d]\/index.html/)
   })
 
   it('keeps clicking on the random navigation link', () => {
+    // confirm we see the main page with the "Index page" text
+    // https://on.cypress.io/contains
     cy.contains('main', 'Index page')
     cy.log('**random navigation**')
+    // while there are navigation links
+    // click on a randomly selected link
+    // Tip: use the recurse function
+    // You can recurse while there are navigation links
+    // using the "post" callback to click on a random one
     recurse(
       () => cy.get('nav a').should(Cypress._.noop),
       ($a) => $a.length === 0,
       {
         log: false,
         post() {
-          cy.get('nav a')
-            .then(($a) => Cypress._.sample($a.toArray()))
-            .click()
+          cy.get('nav a').sample().click()
         },
       },
     )
-    // confirm there are no more links to follow
+    // confirm there are no more navigation links to follow
+    // https://on.cypress.io/get
     cy.get('nav a').should('not.exist')
 
     cy.log('**return to the home page**')
+    // while the location URL is not /public/index.html
+    // keep going back using the cy.go command
+    // https://on.cypress.io/location
+    // https://on.cypress.io/go
     recurse(
       () => cy.location('pathname'),
-      (pathname) => pathname.endsWith('/menu/index.html'),
+      (pathname) => pathname.endsWith('/public/index.html'),
       {
         log: false,
         post() {
@@ -52,6 +63,8 @@ describe('Menu', () => {
         },
       },
     )
+    // confirm we are back on the root page
+    // https://on.cypress.io/contains
     cy.contains('main', 'Index page')
   })
 })
